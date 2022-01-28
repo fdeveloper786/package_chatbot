6e50b3f359b2ccd833730d40436219665efacd93
Flutter Developer <fdeveloper786@gmail.com>
2022-01-29 00:17:14 +0530

    made some changes

---
+++ test/chatbot_package_test.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: missing_required_param, await_only_futures
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:chatbot_package/chatbot_package.dart';
+
+void main() {
+  test('adds one to input values', () {
+    CustomButton(child: Text('Click me'), onPressed: null, style: null);
+  });
+}
+
+
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(CustomButton(
+//       child: null,
+//       onPressed: null,
+//       style: null,
+//     ));
+//   });
+// }
